1c635585b5f2a19f660ca4f397e8bca0b61d0fa2
Aman Madd <29maddhesiaaman@gmail.com>
2023-06-19 22:47:20 +0530

    Added comments and connected the database also

--- controllers/items_controller.js
+++ controllers/items_controller.js
@@ -1,24 +1,28 @@
 const Items = require('../models/items')
 
+
+// ---------This is the action for getting all the items from the database
 module.exports.allItems=function(req,res){
-   return res.status(200).json({
+    return res.status(200).json({
         success:true,
         data:['Items']
-   })
+    })
 }
 
 
 
 
+// ---------This is the action for getting a specific item from the database
 module.exports.specificItem=function(req,res){
-   return res.status(200).json({
+    return res.status(200).json({
         success:true,
         data:'Specific Item'
-   })
+    })
 }
 
 
 
+// ---------This is the action for creating an item and saving it into the database
 module.exports.createItem=function(req,res){
    return res.status(200).json({
         success:true,
@@ -28,7 +32,7 @@ module.exports.createItem=function(req,res){
 
 
 
-
+// ------------This is the action for updating a specific item into the database
 module.exports.updateItem=function(req,res){
     return res.status(200).json({
         success:true,
@@ -38,6 +42,8 @@ module.exports.updateItem=function(req,res){
 
 
 
+
+// ------------------This is the action for deleting a specific item from the database
 module.exports.deleteItem=function(req,res){
    return res.status(200).json({
         success:true,
--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 require('dotenv').config();
+// ---------------Requiring the express library
 const express = require('express');
+
+// ---------------Creating the server
 const app=express();
+
+// ----------------Requiring the database configuration
 const DB=require("./config/mongoose")
+
+// ---------------The port on which the database will run
 const port=process.env.PORT||3000;
 
+// ------------Requiring the middleware for parsing the json payloads coming from frontend
+// --------------- so that we can access the data using req.body.key
 app.use(express.json());
 
 
 
-
+// -----------This is the middleware which basically instructs the server to go and
+// ----------look up into items file inside routes folder whenever there comes a request
+// ------------with baseurl + api
 app.use('/api',require('./routes/items.js'));
+
+// -------------For any non existing url we simply revert back with a simple message of wrong url
 app.all('*',(req,res)=>{
     return res.status(404).json({
         message:'Wrong url is being hit'
     })
 });
+
+
+// ---------Finally starting the server here
 app.listen(port,(err)=>{
     if(err){
         console.log("Error in running the server and the error is :-",err);
