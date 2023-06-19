15ef901bdb9968dc1680c604d5b297872154030f
Aman Madd <29maddhesiaaman@gmail.com>
2023-06-19 23:19:25 +0530

    Done with fetching a specific item from the database

--- controllers/items_controller.js
+++ controllers/items_controller.js
@@ -1,4 +1,5 @@
-const Items = require('../models/items')
+const Items = require('../models/items');
+const mongoose=require('mongoose')
 
 
 // ---------This is the action for getting all the items from the database
@@ -19,18 +20,43 @@ module.exports.allItems = async function(req,res){
         })
         
     }
-
+    
 }
 
 
 
 
 // ---------This is the action for getting a specific item from the database
-module.exports.specificItem=function(req,res){
-    return res.status(200).json({
-        success:true,
-        data:'Specific Item'
-    })
+module.exports.specificItem = async function(req,res){
+    try {
+        const {id}=req.params;
+        console.log("This is the id",id)
+        const itemId = new mongoose.Types.ObjectId(id);
+        const item = await Items.findOne({_id:itemId});
+        if(item){
+            return res.status(200).json({
+                success:true,
+                Items:item
+            })
+            
+        }
+        else{
+            return res.status(200).json({
+                success:true,
+                message:`No item exists with the id=${id}`
+            })
+            
+        }
+        
+    } catch (error) {
+        console.log("Error in fetching the single item from the database and the error is :-", error);
+        return res.status(500).json({
+            success:true,
+            message:'Internal server error'
+        })
+        
+    }
+    
 }
 
 
